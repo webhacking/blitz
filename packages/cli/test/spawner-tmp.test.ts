647078a6cf72bf0934b9814e08e41c814eb378fb
Rudi Yardley <contact@rudiyardley.com>
2020-03-07 18:09:35 +0700

    Test weird issue with spawn config

---
+++ packages/cli/test/spawner-tmp.test.ts
@@ -0,0 +1,26 @@
+let onSpy: jest.Mock
+
+const spawn = jest.fn(() => {
+  let localcb = () => {}
+  onSpy = jest.fn(function on(_: string, cb: () => void) {
+    localcb = cb
+  })
+  setTimeout(() => {
+    localcb && localcb()
+  }, 1000)
+  return {on: onSpy}
+})
+
+jest.doMock('child_process', () => ({spawn}))
+
+import {listFiles} from '../src/spawner-tmp'
+
+describe('list', () => {
+  it('should run spawn', async () => {
+    await listFiles(() => {
+      console.log('callback')
+    })
+    expect(spawn).toHaveBeenCalledWith('ls', ['-l'], {stdio: 'inherit'})
+    expect(onSpy).toHaveBeenCalled()
+  })
+})
